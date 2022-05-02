ce2ba9242459799fef9788e574bc7e0da850dcd9
albert.bikmukhametov <albert.bikmukhametov@tel-int.ru>
2022-05-03 01:38:38 +0300

    test

--- task-03/frontend/src/CitiesApi.js
+++ task-03/frontend/src/CitiesApi.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API_URL = 'http://localhost/api/v1/lib/issue_city/';
+const API_URL = 'http://192.168.97.76:8000/api/v1/lib/issue_city/';
 
 export default class CityManager {
 
